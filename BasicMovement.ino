e94c7bd8e83ec8acbf7313d8a25e10979141fa17
bel-lau <laurenobell@yahoo.com>
2016-03-14 13:54:53 -0400

    Update BasicMovement.ino

--- BasicMovement.ino
+++ BasicMovement.ino
@@ -1,40 +1,16 @@
-#include <mxapi.h>
-#include <servo.h>
-#include <ms_delay.h>
-#include <lcd.h>
-#include <adc.h>
+#include <Servo.h>
 
-int main(void)  
+Servo servo_R;
+Servo servo_L;
+
+void setup(void)
 {
-    servo_init();       //Initialize servo motor control
-    
-    while(1==1)  
-    {
-        if(pin_value(PORT_B0)==HIGH)
-        {
-            servo_robot(REVERSE, 50);
-            delay_ms(1000);
-            servo_robot(SPIN_RIGHT, 100);
-            delay_ms(500);
-        }
-        else if(pin_value(PORT_D7)==HIGH) 
-        { 
-            servo_robot(REVERSE, 50);
-            delay_ms(1000);
-            servo_robot(SPIN_LEFT, 100);
-            delay_ms(500);
-        }
-        else if(adc_read(ADC5)>300) 
-        {  
-            servo_robot(REVERSE, 100);
-            delay_ms(1000);
-            servo_robot(SPIN_RIGHT, 50);
-            delay_ms(100);
-        }
-        else 
-        { 
-            servo_robot(FORWARD, 100);
-        }  
-        delay_ms(10);
-    } 
-} 
+  servo_R.attach(10);
+  servo_L.attach(9); 
+}
+
+void loop(void)
+{
+  servo_R.write(0);
+  servo_L.write(180); 
+}
